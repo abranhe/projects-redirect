e9412899bb27644110c924f63d12cf26a54a207c
Carlos Abraham <abraham@abranhe.com>
2019-01-18 04:15:37 -0500

    adding 404 page with redirect page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './index.css';
 
 const info = {
@@ -9,16 +9,39 @@ const info = {
   http: 'https://'
 }
 
+const Default = () => (
+    <div>
+        <code>projects.abranhe.com{info.currentURL}</code>
+    </div>
+);
+
+const Test = () => (
+    <div>
+        <code>projects.abranhe.com{info.currentURL}</code>
+    </div>
+);
+
+const NoMatch = () => (
+    <div>
+        <code>projects.abranhe.com{info.currentURL}</code>
+        <Route  path="*" component={() => {
+            window.location = info.http + info.to + info.currentURL
+        }}/>
+    </div>
+);
+
 class App extends Component {
   render() {
     return (
         <Router>
         <div className="app">
         <header className="app-header">
-        <h3>Redirecting to </h3> <code>projects.abranhe.com{info.currentURL}</code>
-        <Route  path="*" component={() => {
-            window.location = info.http + info.to + info.currentURL
-        }}/>
+        <h3>Redirecting to </h3> 
+        <Switch>
+            <Route path="/" exact component={Default}/>
+            <Route path="/test" component={Test}/>
+            <Route component={NoMatch} />
+      </Switch>
         </header>
         </div>
         </Router>
