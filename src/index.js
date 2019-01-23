1fbea7252a20848bd2231cfd37427b72d93be22d
Carlos Abraham <abraham@abranhe.com>
2019-01-22 19:59:28 -0500

    adding a pkg wraper to redirect

--- src/index.js
+++ src/index.js
@@ -1,15 +1,35 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Redirecter  from './redirecting';
+import Redirector from 'react-redirector';
 import './index.css';
+
+const info = {
+    currentURL: window.location.pathname,
+    to: 'projects.abranhe.com',
+    http: 'https://'
+}
+
+const NoMatch = () => {
+    return(
+        <div className="app">
+        <header className="app-header">
+            <h3>Redirecting to</h3>
+            <div>
+            <code>projects.abranhe.com{info.currentURL}</code>
+            <Redirector to={info.http + info.to + info.currentURL}/>
+        </div>
+        </header>
+        </div>  
+    );
+}
 
 class App extends Component {
   render() {
     return (
         <Router>
             <Switch>
-                <Route component={Redirecter} />
+                <Route component={NoMatch} />
             </Switch>
         </Router>
     );
