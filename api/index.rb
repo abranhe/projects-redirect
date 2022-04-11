b85a906efd2b0d62be4fc24d605c925be02a8657
Abraham Hernandez <abraham@abranhe.com>
2022-04-10 21:23:41 -0400

    tweaks

--- api/index.rb
+++ api/index.rb
@@ -1,15 +1,7 @@
 Handler = Proc.new do |req, res|
-  res.status = 200
-  res['Content-Type'] = 'text/text; charset=utf-8'
-
   domain = 'abranhe.com'
   path = req.path_info
-
-  if path == '/'
-    res.status = 301
-    res['Location'] = "https://#{domain}"
-  else
-    res.status = 301
-    res['Location'] = "https://projects.#{domain}#{path}"
-  end
+  
+  res.status = 301
+  res['Location'] = path == '/' ? "https://#{domain}" : "https://projects.#{domain}#{path}"
  end
